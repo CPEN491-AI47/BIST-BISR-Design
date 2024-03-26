2a0f6e8378726c57e4118d6cbcbe52c7c6b1d553
Th3br0k3nh4nD <65325884+Th3br0k3nh4nD@users.noreply.github.com>
2024-03-25 17:39:48 -0700

    Update recompute_unit.sv

--- BISR/RecomputeUnitApproach/recompute_unit.sv
+++ BISR/RecomputeUnitApproach/recompute_unit.sv
@@ -1,1 +1,35 @@
+module recompute_unit
+    #(parameter WORD_SIZE = 16)(
+    input clk,
+    input rst,
+    input [ROWS-1] faultyRowIn,
+    input [COLS-1] faultyColIn,
+    input [WORD_SIZE-1:0] Weight,
+    input [WORD_SIZE-1:0] TopIn,
+    input [WORD_SIZE-1:0] LeftIn,
 
+    output reg [WORD_SIZE-1:0] BottomOut,
+    output reg [WORD_SIZE-1:0] RightOut,
+    output reg [ROWS-1] faultyRowOut,
+    output reg [COLS-1] faultyColOut,
+);
+
+    reg [WORD_SIZE-1:0] Accumulator;
+
+
+    always @(posedge clk, posedge rst) begin
+        if(rst) begin
+            BottomOut   <= 0;
+            RightOut    <= 0;
+            Accumulator <= 0;
+        end
+        else begin
+            RightOut    <= LeftIn;
+            BottomOut   <= Accumulator;
+            Accumulator <= Weight*LeftIn + TopIn;
+            faultyRowOut <= faultyRowIn;
+            faultyColOut <= faultyColIn;
+        end   
+    end
+    
+endmodule
